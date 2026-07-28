5faa1f7df1af05ada690cc381d6bcdd1c1802de5
Vedant Ralekar <ralekaru@gmai.com>
2026-07-28 16:35:54 +0530

    files added

--- retrieval/bm25_search.py
+++
@@ -1,8 +0,0 @@
-from langchain_community.retrievers import BM25Retriever
-
-def create_bm25(chunks):
-
-    retriever = BM25Retriever.from_documents(chunks)
-    retriever.k = 5
-
-    return retriever
